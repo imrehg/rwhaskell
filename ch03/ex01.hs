f6719ec1e5e9a654c33eec834332adb8a3f08d8b
Gergely Imreh <imrehg@gmail.com>
2010-12-13 16:56:57 +0800

    Excercise 1: number of elements of a list

---
+++ ch03/ex01.hs
@@ -0,0 +1,8 @@
+-- file ch03/ex01.hs
+-- Write a function that computes the number of elements in a list. 
+-- To test it, ensure that it gives the same answers as the standard
+-- length function.
+elements :: [a] -> Int
+elements [] = 0
+elements (x:xs) = elements(xs) + 1
+
